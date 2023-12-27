04e1cb0c2b197d9dffa3ea94a5cfcbcbd5732365
Andrew-Karvelis <akarvelis.work@gmail.com>
2023-12-28 10:39:06 +1100

    Added text to display what the user has searched for

--- movies.js
+++ movies.js
@@ -1,18 +1,25 @@
-const movieListElem = document.querySelector(".movie__list")
+const movieListElem = document.querySelector(".movie__list");
 const id = localStorage.getItem("id");
-//950c424d
+
+let searchValue = document.querySelector(".search__value");
+
+//950c424d API KEY
 
 async function searchTerm(event)  {
-    const movieId = event.target.value
-    getMovies(movieId)
+    const movieId = event.target.value;
+    getMovies(movieId);
+    searchValue.innerHTML = movieId
+}
+
+function searchResult(searchValue) {
+    return `<div class="results__bar">
+                <h2>Search results for: <span class="purple search__value" onchange="searchTerm(event)">${searchValue}</span></h2>`;
 }
 
 async function getMovies(movieId) {
     const movies = await fetch(`https://www.omdbapi.com/?apikey=950c424d&s=${movieId}`);
-    const movieData = await movies.json()
-    // return await movies.json()
-    movieListElem.innerHTML = movieData.Search.map((movie) => getMovieDescription(movie)).join("")
-    console.log(movieData)
+    const movieData = await movies.json();
+    movieListElem.innerHTML = movieData.Search.map((movie) => getMovieDescription(movie)).join("");
 }
 
 function getMovieDescription(movie) {
@@ -30,6 +37,6 @@ function getMovieDescription(movie) {
             <p class="movie__description movie__year"><span class="movie__description--heading">Year:</span> ${movie.Year}</p>
             <p class="movie__description movie__year"><span class="movie__description--heading">IMDB ID:</span> ${movie.imdbID}</p>
         </div>
-    </div>`
+    </div>`;
 }
 getMovies();
